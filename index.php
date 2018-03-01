36d834b76d560f9e786f584ea96878062882f449
Marcelo Souza <marceloamorsouza@yahoo.com.br>
2018-02-28 21:58:04 -0300

    Classe Usuario criada

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 
 require_once("config.php");
 
-$sql = new Sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuario");
-
-echo json_encode($usuarios);
-
+	$root = new Usuario();
+    $root->loadById(3);
+    print $root;
 
  ?>
