4f845507cad35a36d37847c35c6b5d5681d2943f
Marcelo Souza <marceloamorsouza@yahoo.com.br>
2018-03-01 02:52:16 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 
 require_once("config.php");
 
-	$root = new Usuario();
-    $root->loadById(3);
-    print $root;
+	//carrega apenas 1 usuario
+	//$root = new Usuario();
+    //$root->loadById(3);
+  //  echo $root;
 
+	// carrega uma lista de usuario
+
+	//$lista = Usuario::getList();
+
+	//echo json_encode($lista);
+
+
+	//carregar lista de usuarios buscando pelo login
+
+	//$search = Usuario::search("Ad");
+
+	//echo json_encode($search);
+
+	//carregar usuario com autenticação
+
+	$usuario = new Usuario();
+	$usuario->login("Marcelo Souza", "7777");
+
+	echo $usuario;
  ?>
