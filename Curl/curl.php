724546a9888af7a51c7ea2ccd63428fe7b5c9a39
Marcelo Souza <marceloamorsouza@yahoo.com.br>
2018-03-07 17:22:49 -0300

    CURL

---
+++ Curl/curl.php
@@ -0,0 +1,22 @@
+<?php 
+
+$cep = "01310-100";
+
+$link = "http://viacep.com.br/ws/$cep/json/";
+
+$ch = curl_init($link); 
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+print_r($data);
+
+
+ ?>
